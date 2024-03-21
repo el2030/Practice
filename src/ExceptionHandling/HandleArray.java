614216e7b897c2416c4141b9083071db1edbe33b
el2030 <108585078+el2030@users.noreply.github.com>
2024-03-21 17:11:16 -0400

    Update HandleArray.java

--- src/ExceptionHandling/HandleArray.java
+++ src/ExceptionHandling/HandleArray.java
@@ -1,15 +1,20 @@
 package ExceptionHandling;
 
+import java.util.Scanner;
+
 public class HandleArray {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         int[] intArray = {0,1,2,3,4,5,6,7,8,9};
-        int eleven = 0;
-        try{
-            eleven = intArray[10];
-            System.out.println("Array 11 is: " + intArray[10]);
-        } catch (ArrayIndexOutOfBoundsException a){
-            a.printStackTrace();
+        int result = 0;
+        System.out.println("Please input a number");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if (num>9){
+            throw new Exception("Num cannot be bigger than 9");
+        } else {
+            result = intArray[num];
+            System.out.println("Array " + num + " is " + result);
         }
-        System.out.println("Array 5 is: " + intArray[5]);
+        System.out.println("Array 5 is " + intArray[5]);
     }
 }
